d15a5bb8eae5ea23f981c6d3abb91d033e0afe43
nickcabuno <nickcabuno17@gmail.com>
2021-06-09 01:51:15 -0400

    car

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1D8HB48N18F129086',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '2HGFB6E55DH787865',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+
+    },
+    {
+        vin: 'JKBVNAP183A000151',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
